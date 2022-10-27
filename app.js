86db66979a1c11abad8e6b562f85974e5f1990d9
shch989 <tankpop11@gmail.com>
2022-10-27 19:29:54 +0900

    기본 환경 구축

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 3000
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, './', 'views', '404.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
