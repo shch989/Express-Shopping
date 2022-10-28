dfae2ca2f667bfdb3b8d52dd37544ce452d2e7dd
shch989 <tankpop11@gmail.com>
2022-10-28 17:20:40 +0900

    일부 수정

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-const express = require('express')
-const path = require('path')
-const bodyParser = require('body-parser')
+const path = require('path');
 
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.set('view engine', 'pug')
-app.set('views', 'views')
+const app = express();
 
-const adminData = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(express.static(path.join(__dirname, 'public')))
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/admin', adminData.routes)
-app.use(shopRoutes)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.render('404', {pageTitle: '404'});
-})
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(3000);
